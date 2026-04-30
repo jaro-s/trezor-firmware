42a5ec9d1707c8f923802c7c0406456148057843
jaro <jaroslav@sedenka.cz>
2026-04-30 22:39:40 +0200

    more tests for kv-auth

--- core/tests/test_apps.common.kv_auth.py
+++ core/tests/test_apps.common.kv_auth.py
@@ -4,11 +4,8 @@ from common import *  # isort:skip
 from ubinascii import unhexlify
 
 from kv_vectors import (
-    DERIVED_INDEX_KEY_HEX,
-    DERIVED_RECORD_ID_VECTORS,
-    DERIVED_SIGN_PUBLIC_KEY_HEX,
-    DERIVED_SIGN_SECRET_KEY_HEX,
-    SEED_HEX,
+    KV_AUTH_DERIVATION_VECTORS,
+    KV_AUTH_MAX_KEY,
 )
 
 from apps.common import kv_auth
@@ -17,25 +14,37 @@ from apps.common.paths import AlwaysMatchingSchema
 
 
 class TestKvAuth(unittest.TestCase):
+    def _keychain(self, seed_hex):
+        return Keychain(unhexlify(seed_hex), "secp256k1", [AlwaysMatchingSchema])
+
     def test_derivation_vectors(self):
-        keychain = Keychain(unhexlify(SEED_HEX), "secp256k1", [AlwaysMatchingSchema])
-        self.assertEqual(
-            kv_auth.sign_private_key(keychain),
-            unhexlify(DERIVED_SIGN_SECRET_KEY_HEX),
-        )
-        self.assertEqual(
-            kv_auth.sign_public_key(keychain),
-            unhexlify(DERIVED_SIGN_PUBLIC_KEY_HEX),
-        )
-        self.assertEqual(kv_auth.index_key(keychain), unhexlify(DERIVED_INDEX_KEY_HEX))
+        for item in KV_AUTH_DERIVATION_VECTORS:
+            keychain = self._keychain(item["seed_hex"])
+            sign_private_key = kv_auth.sign_private_key(keychain)
+            index_key = kv_auth.index_key(keychain)
+            self.assertEqual(sign_private_key, unhexlify(item["sign_secret_key_hex"]))
+            self.assertEqual(
+                kv_auth.sign_public_key(keychain),
+                unhexlify(item["sign_public_key_hex"]),
+            )
+            self.assertEqual(index_key, unhexlify(item["index_key_hex"]))
+            self.assertNotEqual(sign_private_key, index_key)
 
     def test_record_id_vectors(self):
-        keychain = Keychain(unhexlify(SEED_HEX), "secp256k1", [AlwaysMatchingSchema])
-        for item in DERIVED_RECORD_ID_VECTORS:
-            self.assertEqual(
-                kv_auth.record_id(keychain, item["key"]),
-                unhexlify(item["record_id_hex"]),
-            )
+        for derivation in KV_AUTH_DERIVATION_VECTORS:
+            keychain = self._keychain(derivation["seed_hex"])
+            for item in derivation["record_id_vectors"]:
+                self.assertEqual(
+                    kv_auth.record_id(keychain, item["key"]),
+                    unhexlify(item["record_id_hex"]),
+                )
+
+    def test_record_id_rejects_invalid_keys(self):
+        keychain = self._keychain(KV_AUTH_DERIVATION_VECTORS[0]["seed_hex"])
+        with self.assertRaises(ValueError):
+            kv_auth.record_id(keychain, "")
+        with self.assertRaises(ValueError):
+            kv_auth.record_id(keychain, KV_AUTH_MAX_KEY + "a")
 
 
 if __name__ == "__main__":
